e003b7ae58b58ea9bb94b067c68d0f2b4ccdef9b
sfinnqs <email@sfinnqs.org>
2019-09-27 13:54:28 -0500

    License headers

--- src/main/kotlin/com/voichick/cpn/CpnListener.kt
+++ src/main/kotlin/com/voichick/cpn/CpnListener.kt
@@ -1,3 +1,21 @@
+/**
+ * ColoredPlayerNames - A Bukkit plugin for changing name colors
+ * Copyright (C) 2019 sfinnqs
+ *
+ * This file is part of ColoredPlayerNames.
+ *
+ * ColoredPlayerNames is free software; you can redistribute it and/or modify it
+ * under the terms of version 3 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <https://www.gnu.org/licenses>.
+ */
 package com.voichick.cpn
 
 import org.bukkit.ChatColor.*
